a871a7013fda99f70dd22e78a7720c84a6fec932
Kevin Rodriguez <_@kevinrodriguez.io>
2021-12-27 02:41:11 -0600

    feat: more rules

--- src/commands/generateArt.ts
+++ src/commands/generateArt.ts
@@ -11,7 +11,7 @@ import { padNumber } from "../lib/tools/string.js";
 import mkdirp from "mkdirp";
 
 const cpuCount = os.cpus().length;
-const limit = pLimit(cpuCount);
+const limit = pLimit(cpuCount); // pLimit(1); // pLimit(cpuCount);
 
 const PAINTER_SCRIPT = "./bin/src/scripts/painter.js";
 
@@ -64,6 +64,7 @@ const reRollIfException = (
   chance: Chance.Chance = new Chance()
 ) => {
   while (exceptions.includes(items[layer].pickedLayerItem)) {
+    console.log("Rerolling layer", layer);
     items[layer].pickedLayerItem = chance.weighted(
       config[layer].items,
       config[layer].weights
@@ -91,6 +92,31 @@ const pickLayers = (
 
   // TODO: Move this into a re-ordering worker plugin.
   //#region JungleCats Lionesses
+
+  if (items["Skin"].pickedLayerItem === "Zombie") {
+    reRollIfException(
+      "Eyes",
+      [
+        "Blue",
+        "Cat Eyes",
+        "Dragon Eyes",
+        "Glowing Blue",
+        "Glowing White",
+        "Glowing Yellow",
+        "Green",
+        "Orange",
+        "Red",
+        "Snake Eyes",
+        "Solana Eyes",
+        "Yellow",
+      ],
+      {
+        items,
+        config,
+      }
+    );
+  }
+
   if (items["Head"].pickedLayerItem === "Cleopatra") {
     reRollIfException(
       "Eyes",
@@ -104,6 +130,8 @@ const pickLayers = (
         "Soundwave Goggles",
         "Steam Punk",
         "VR",
+        "Laser Eyes",
+        "Ray Eyes",
       ],
       { items, config }
     );
@@ -121,6 +149,7 @@ const pickLayers = (
         "Nerd Glasses",
         "Opera Mask",
         "Pit Vipers",
+        "Heart Glasses",
         "Steam Punk",
         "VR",
       ],
@@ -166,16 +195,16 @@ const pickLayers = (
       config,
     });
   } else if (
-    ["Green Mushroom Hat", "Purple Mushroom Hat"].includes(
-      items["Head"].pickedLayerItem
-    )
+    items["Head"].pickedLayerItem === "Green Mushroom Hat" ||
+    items["Head"].pickedLayerItem === "Purple Mushroom Hat"
   ) {
     reRollIfException("Eyes", ["Ski Goggles", "Soundwave Goggles", "VR"], {
       items,
       config,
     });
   } else if (
-    ["Pink Visor", "Blue Visor"].includes(items["Head"].pickedLayerItem)
+    items["Head"].pickedLayerItem === "Pink Visor" ||
+    items["Head"].pickedLayerItem === "Blue Visor"
   ) {
     reRollIfException("Eyes", ["Soundwave Goggles", "Ski Goggles"], {
       items,
@@ -205,24 +234,28 @@ const pickLayers = (
       items,
       config,
     });
-  } else if (items["Eyes"].pickedLayerItem === "Opera Mask") {
+  }
+
+  if (items["Eyes"].pickedLayerItem === "Opera Mask") {
     reRollIfException("Mouth", ["Gas Mask", "Microphone", "Scuba"], {
       items,
       config,
     });
-    reRollIfException("Nose", ["Stud", "Ring", "Septum"], {
+    reRollIfException("Nose", ["Stud", "Ring", "Septum", "Butterfly"], {
       items,
       config,
     });
   } else if (
-    ["Laser Eyes", "Ray Eyes"].includes(items["Eyes"].pickedLayerItem)
+    items["Eyes"].pickedLayerItem === "Laser Eyes" ||
+    items["Eyes"].pickedLayerItem === "Ray Eyes"
   ) {
     reRollIfException("Nose", ["Butterfly"], {
       items,
       config,
     });
-  } else if (items["Eyes"].pickedLayerItem === "Pit Vipers") {
-  } else if (items["Mouth"].pickedLayerItem === "Gas Mask") {
+  }
+
+  if (items["Mouth"].pickedLayerItem === "Gas Mask") {
     reRollIfException("Nose", ["Stud", "Ring", "Septum"], {
       items,
       config,
@@ -233,22 +266,35 @@ const pickLayers = (
       items,
       config,
     });
-  } else if (items["Skin"].pickedLayerItem === "Zombie") {
+  } else if (items["Mouth"].pickedLayerItem === "Microphone") {
+    reRollIfException("Accessories", ["Bone Necklace", "Sea Shell"], {
+      items,
+      config,
+    });
+  }
+
+  if (items["Accessories"].pickedLayerItem === "Egyptian Necklace") {
     reRollIfException(
-      "Eyes",
+      "Top",
       [
-        "Blue",
-        "Cat Eyes",
-        "Dragon Eyes",
-        "Glowing Blue",
-        "Glowing White",
-        "Glowing Yellow",
-        "Green",
-        "Orange",
-        "Red",
-        "Snake Eyes",
-        "Solana Eyes",
-        "Yellow",
+        "Yellow Shirt",
+        "White Tracksuit",
+        "Red Poncho",
+        "Purple Shirt",
+        "Purple Puffer Jacket",
+        "Puffer Vest",
+        "Pink Tracksuit",
+        "Green Tracksuit",
+        "Green Shirt",
+        "Green Puffer Jacket",
+        "Green Poncho",
+        "Dress",
+        "Cream Puffer Jacket",
+        "Cargo Vest",
+        "Blue Tracksuit",
+        "Blue Shirt",
+        "Black Puffer Jacket",
+        "Black Shirt",
       ],
       {
         items,
@@ -256,10 +302,14 @@ const pickLayers = (
       }
     );
   }
+
   //#endregion
 
   //#region Ordering
-  if (items["Accessories"].pickedLayerItem === "Butterfly Wings") {
+  if (
+    items["Accessories"].pickedLayerItem === "Butterfly Wings" ||
+    items["Accessories"].pickedLayerItem === "Wings"
+  ) {
     for (const layer of Object.keys(items)) {
       if (layer !== "Background") {
         items[layer].priority += 1;
@@ -276,20 +326,47 @@ const pickLayers = (
     items["Pseudo"] = {
       pickedLayerItem: "Scuba",
       priority: 1,
-    }
+    };
   }
-  if (items["Nose"].pickedLayerItem === "Butterfly" && items["Accessories"].pickedLayerItem !== "Butterfly Wings") {
-    items["Accessories"].priority = 8; // Head is the topmost, and it's 7
+
+  if (
+    items["Mouth"].pickedLayerItem === "Microphone" &&
+    items["Eyes"].pickedLayerItem !== "Ray Eyes" &&
+    items["Eyes"].pickedLayerItem !== "Laser Eyes"
+  ) {
+    items["Mouth"].priority =
+      Object.entries(items)
+        .map((i) => i[1].priority)
+        .sort((a, b) => b - a)[0] + 1;
   }
   if (
-    ["Ski Goggles", "Ski Goggles"].includes(
-      items["Accessories"].pickedLayerItem
-    )
+    items["Mouth"].pickedLayerItem === "Sneakers" &&
+    items["Head"].pickedLayerItem === "Cleopatra"
   ) {
-    items["Accessories"].priority = 8; // Head is the topmost, and it's 7
+    items["Mouth"].priority =
+      Object.entries(items)
+        .map((i) => i[1].priority)
+        .sort((a, b) => b - a)[0] + 1;
+  }
+  if (items["Accessories"].pickedLayerItem === "Egyptian Necklace") {
+    items["Accessories"].priority = items["Top"].priority - 0.1; // Behind by little.
+  }
+  if (items["Nose"].pickedLayerItem === "Butterfly") {
+    items["Nose"].priority =
+      Object.entries(items)
+        .map((i) => i[1].priority)
+        .sort((a, b) => b - a)[0] + 1;
+  }
+  if (
+    items["Eyes"].pickedLayerItem === "Ray Eyes" ||
+    items["Eyes"].pickedLayerItem === "Laser Eyes"
+  ) {
+    items["Eyes"].priority =
+      Object.entries(items)
+        .map((i) => i[1].priority)
+        .sort((a, b) => b - a)[0] + 1;
   }
   //#endregion
-
   return items;
 };
 
@@ -371,12 +448,14 @@ const generateNFT = async (
   i: number,
   { layersPath, outputFormat, outputPath }: GenerateArtInput,
   layersConfig: Record<string, LayerConfigItem>,
-  jsonTemplate: NFTMetaData
+  jsonTemplate: NFTMetaData,
+  logger?: Logger
 ) => {
   // TODO: Create a file system cache since this doesn't regularly change during generation
   const pickedLayerItems = await Promise.all(
     getLayerItemUris(layersPath, pickLayers(layersConfig))
   );
+  logger?.log(`[${i + 1}] - ${JSON.stringify(pickedLayerItems)}`);
   const [imageUri, { jsonTemplateForItem, metaDataUri }] = await Promise.all([
     writeImage(i, pickedLayerItems, outputFormat, outputPath),
     writeMetadataJson(
@@ -417,7 +496,8 @@ export const createGenerateArtCommand =
               i,
               input,
               layersConfig,
-              jsonTemplate
+              jsonTemplate,
+              logger
             );
             return result;
           } catch (e) {
